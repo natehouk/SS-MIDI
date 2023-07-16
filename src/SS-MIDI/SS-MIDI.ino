c4ec191408b4c0613c06abf5d6052adfe69942fc
Nate Houk <nate.houk@gmail.com>
2023-07-16 10:04:08 +0200

    Refactor

--- src/SS-MIDI/SS-MIDI.ino
+++ src/SS-MIDI/SS-MIDI.ino
@@ -4,6 +4,8 @@
 #include <Midier.h>
 #include <Fsm.h>
 #include <Vector.h>
+#include <SD.h>
+#include <SPI.h>
 
 #define DEBUG true
 #define PATTERNS 4
@@ -14,6 +16,11 @@
 #define OFF 0
 #define PPQN 24
 #define ONE_MINUTE 60000000
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+const int chipSelect = 10;
 
 const int debounceDelay = 10;  // iterations to wait until pin is stable
 bool lastPlayButtonState = LOW;  // Last state of the button
@@ -298,7 +305,7 @@ void blinkLED() {
 }
 
 void sendAllNotesOff(byte channel) {
-  log("sendAllNotesOff()");
+  // log("sendAllNotesOff()");
 
   // Send all notes off
   MIDI.sendControlChange(123, 0, channel);
@@ -395,6 +402,9 @@ void playStep() {
 
 void setup() {
   Serial.begin(9600);
+    while (!Serial) {
+      continue; // wait for serial port to connect.
+  }
   Serial.println("setup()");
   randomSeed(analogRead(0));
   pinMode(ledPin, OUTPUT);
@@ -439,10 +449,62 @@ void setup() {
     },
   };
 
+  Serial.print("\nInitializing SD card...");
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  if (volumesize < 8388608ul) {
+    Serial.print("Volume size (bytes): ");
+    Serial.println(volumesize * 512);        // SD card blocks are always 512 bytes
+  }
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 2;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+  
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
   // Start master clock using timer interrupt
   clk.begin(pulse, ONE_MINUTE / PPQN / bpm);
-
-  sequencer.trigger(STOP);
 }
 
 // debounce returns the state when the switch is stable
